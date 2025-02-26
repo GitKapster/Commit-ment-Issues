9a6fd64f72b4041328fe873fa7822c51a8b8f188
Kapi <kapi251003@gmail.com>
2025-02-26 13:57:47 +0000

    Added Forums and Posts

--- coursework/init.sql
+++ coursework/init.sql
@@ -1,18 +1,42 @@
 CREATE DATABASE IF NOT EXISTS AimTrainer;  --have the if not statement incase someone else runs it and it tries creating the database twice
 USE AimTrainer;  -- This selects which database to create all the tables in
 
-CREATE TABLE Users (
+--User Accounts Info
+CREATE TABLE Users ( 
   UserID int PRIMARY KEY,
   Username varchar(25),
   Password varchar(25)
 );
 
+--Leaderboards Info
 CREATE TABLE Leaderboard (
   TaskID int,
   UserID int,
   Username varchar(25),
   Score int,
   FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+
+--Table for Forums
+CREATE TABLE Forums (
+  ForumID int,
+  UserID int,
+  Title varchar (25),
+  Date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (ForumID),
+  KEY UserID (UserID)
+);
+
+--Table for Posts (inside of Forums)
+CREATE TABLE Posts (
+  ThreadID int,
+  PostID int,
+  UserID int,
+  Title varchar (25),
+  Body text NOT NULL,
+  PRIMARY KEY (PostID),
+  KEY ForumID (ForumID),
+  Key UserID (UserID)
 );
 
 
