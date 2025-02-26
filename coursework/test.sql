4c3d368443332acec43ef3579cfb06b4b0f2aa84
Kapi <kapi251003@gmail.com>
2025-02-26 12:54:50 +0000

    Added the init file so the database loads on docker startup

--- coursework/test.sql
+++ coursework/test.sql
@@ -1,17 +1,19 @@
 CREATE DATABASE AimTrainer;
 
+USE AimTrainer;  -- This selects which database to create all the tables in
+
 CREATE TABLE Users (
-  UserID int,
+  UserID int PRIMARY KEY,
   Username varchar(25),
   Password varchar(25)
 );
-
 
 CREATE TABLE Leaderboard (
   TaskID int,
   UserID int,
   Username varchar(25),
-  Score int
+  Score int,
+  FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
 
