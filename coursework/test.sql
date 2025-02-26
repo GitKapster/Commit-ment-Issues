fdb1fa5e7bb2190ee15058b8770bb498be8f329b
Kapi <kapi251003@gmail.com>
2025-02-26 12:47:31 +0000

    Added more tables

--- coursework/test.sql
+++ coursework/test.sql
@@ -1,6 +1,18 @@
+CREATE DATABASE AimTrainer;
+
+CREATE TABLE Users (
+  UserID int,
+  Username varchar(25),
+  Password varchar(25)
+);
+
+
 CREATE TABLE Leaderboard (
   TaskID int,
   UserID int,
-  Username varchar(25)
-  Score int,
-)
+  Username varchar(25),
+  Score int
+);
+
+
+
